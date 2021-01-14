573459be79db96efd89c909b1638f41089b9394b
KwonEyng <nan308@naver.com>
2021-01-14 15:17:27 +0900

    Solve Problem

--- 1월 3주차/지형이동.js
+++ 1월 3주차/지형이동.js
@@ -1,44 +1,53 @@
-var land = [[1, 4, 8, 10], [5, 5, 5, 5], [10, 10, 10, 10], [10, 10, 10, 20]]
-var height = 3
-var n = land.length
-var answer = 0;
-var marking = Array(n).fill(0)
-for(var i=0; i<n;i++){
-    marking[i] = Array(n).fill(0)
+function find(x,parent){
+    if (parent[x] == x) return x
+    parent[x] = find(parent[x], parent)
+    return parent[x]
 }
-var dx,dy,q,ht,cnt,cur,x,y,xi,yi
-ht = new Map()
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-cnt = 0
 
-for (var i=0; i<n; i++){
-    for (var j=0; j<n; j++){
-        if (marking[i][j] == 0){
-            cnt += 1
-            q = [[i,j]]
-            while (q.length){
-                cur = q.shift()
-                x = cur[0]; y = cur[1];
-                marking[x][y] = cnt
-                for (var k=0; k<4; k++){
-                    xi = x+dx[k]; yi = y+dy[k]
-                    if (xi >= 0 && xi < n && yi >= 0 && yi < n){
-                        if (marking[xi][yi] == 0){
-                            if (Math.abs(land[xi][yi] - land[x][y]) <= height){
-                                q.push([xi,yi])
-                            }
-                        } else if (marking[xi][yi] != marking[x][y]){
-                            var a,b
-                            a = marking[xi][yi]
-                            b = marking[x][y]
-                            if (a>b){
-                                [a,b] = [b,a]
-                            }
-                            if (ht.get((a,b))){
-                                ht[(a,b)] = Math.min(Math.abs(land[x][y] - land[xi][yi]), ht[(a,b)])
-                            } else{
-                                ht.set((a,b), Math.abs(land[x][y]-land[xi][yi]))
+function merge(parent,ladder){
+    var ans = 0
+    var v,x,y,px,py
+    ladder.forEach((iter)=>{
+        [v,x,y] = iter
+        px = find(x,parent)
+        py = find(y,parent)
+        if (px != py){
+            parent[py] = px
+            ans += v
+        }
+    })
+    
+    return ans
+}
+
+function solution(land, height) {
+    var n = land.length
+    var marking = Array(n).fill(0)
+    for (var i=0; i<n; i++){
+        marking[i] = Array(n).fill(0)
+    }
+    var dx,dy,q,ht,cnt,cur,x,y,xi,yi
+    ht = new Map()
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
+    cnt = 0
+    for (var i=0; i<n; i++){
+        for (var j=0; j<n; j++){
+            if (marking[i][j] == 0){
+                cnt += 1
+                q = [[i,j]]
+                marking[i][j] = cnt
+                while (q.length){
+                    cur = q.shift()
+                    x = cur[0]; y = cur[1];
+                    for (var p=0; p<4; p++){
+                        xi = x+dx[p]; yi = y+dy[p];
+                        if (xi>=0 && xi<n && yi>=0 && yi<n){
+                            if (marking[xi][yi] == 0){
+                                if (Math.abs(land[xi][yi] - land[x][y]) <= height){
+                                    q.push([xi,yi])
+                                    marking[xi][yi] = cnt
+                                }
                             }
                         }
                     }
@@ -46,10 +55,47 @@ for (var i=0; i<n; i++){
             }
         }
     }
+    var a,b,dh
+    for (var x=0; x<n; x++){
+        for (var y=0; y<n; y++){
+            for (var k=0; k<4; k++){
+                xi = x+dx[k]; yi = y+dy[k];
+                if (xi<n && xi>=0 && yi<n && yi>= 0 && marking[xi][yi] != marking[x][y]){
+                    a = marking[xi][yi]
+                    b = marking[x][y]
+                    dh = Math.abs(land[xi][yi] - land[x][y])
+                    if (a>b){
+                        [a,b] = [b,a]
+                    }
+                    if (a in ht){
+                        if (b in ht[a]){
+                            ht[a][b] = Math.min(ht[a][b], dh)
+                        } else{
+                            ht[a][b] = dh
+                        }
+                    } else{
+                        ht[a] = {}
+                        ht[a][b] = dh
+                    }
+                }
+            }
+        }
+    }
+    var ladder = []
+    Object.keys(ht).forEach((x)=>{
+        Object.keys(ht[x]).forEach((y)=>{
+            ladder.push([ht[x][y], x, y])
+        })  
+    })
+    ladder = ladder.sort((a,b)=>{
+        return a[0] - b[0]
+    })
+    
+    var parent = []
+    for (var i=0; i<cnt; i++){
+        parent.push(i)
+    }
+    // return ht
+    return merge(parent,ladder)
+    
 }
-
-
-console.log(marking)
-ht.forEach((value,key)=>{
-    console.log(value, key)
-})
